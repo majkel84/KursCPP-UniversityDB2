88ae9378a800cb703f4dc5e49a34dc88387ee3c4
Adriana <agrzesiuk.7@gmail.com>
2019-01-24 22:47:42 +0100

    Database: Add sorting by PESEL

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,1 +1,8 @@
 #include "DataBase.hpp"
+#include <algorithm>
+
+void Database::sortByPESEL() {
+    std::sort(begin(db_), end(db_), [](Person * one, Person * two) {
+        return one->getPESEL() < two->getPESEL();
+        });
+}
