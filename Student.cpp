234fb4b9652bed41e5894cc63cab70faf87d110e
Jakub Zych <jakub.zych@tieto.com>
2019-01-24 14:43:29 +0100

    Student: Add Student class body and header

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,22 @@
 #include "Student.hpp"
+
+using namespace std;
+
+Student::Student(const string name, 
+                 const string lastName, 
+                 const int64_t pesel, 
+                 const bool gender,      // true - male, false - female
+                 const string address,
+                 const int32_t index) 
+    : Person(name, lastName, pesel, gender, address)
+    , index_(index) {}
+
+int32_t Student::getIndex() const {
+    return index_;
+}
+
+void Student::show() {
+    cout << name_ << " " << lastName_ << " " << pesel_ << " " 
+         << (gender_ ? "male " : "female ") << address_ << " "
+         << index_ << endl;
+}
