779312f0ffbb933439ba28d9dbd935e4f97b7bdc
Jakub Zych <jakub.zych@tieto.com>
2019-01-24 14:44:57 +0100

    Database: Add class header file with functions porototypes

--- includes/DataBase.hpp
+++ includes/DataBase.hpp
@@ -1,1 +1,29 @@
+#include <vector>
+#include <string>
+#include "Student.hpp"
+#include "Employee.hpp"
 
+using namespace std;
+using Persons = vector<Person*>;
+
+class Database {
+    public:
+        void addStudent(const Student & student);
+        void addEmployee(const Employee & employee);
+        void removeByPESEL(const uint32_t pesel);
+        void modifyIncome(const uint newIncome, const uint32_t pesel);
+        void modifyAddress(const string newAddress, const uint32_t pesel);
+        void sortByLastname();
+        void sortByPESEL();
+        void sortByIncome();
+        void searchByLastname(const string lastname, Person* item);
+        void searchByPESEL(const uint32_t pesel, Person* item);
+        void showAll();
+        bool validatePESEL(uint32_t pesel);
+        bool loadFromFile(const string filename);
+        bool saveToFile(const string filename);
+        
+    private:
+        Persons db_;
+};
+        
