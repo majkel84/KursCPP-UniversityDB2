2c4e0b1539dbd1220eb3458fd2cb2788f6543cb2
Adriana <agrzesiuk.7@gmail.com>
2019-01-24 23:35:19 +0100

    Database: Add sorting by lastname

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,6 +1,4 @@
 #include "DataBase.hpp"
-#include <algorithm>
-
 void Database::sortByPESEL() {
     std::sort(begin(db_), end(db_), [](Person * one, Person * two) {
         return one->getPESEL() < two->getPESEL();
@@ -14,5 +12,11 @@ void Database::addItem(Person * person) {
 void Database::showAll() {
     for (auto x : db_)
         x->show();
+}
+
+void Database::sortByLastname() {
+    std::sort(begin(db_), end(db_), [](const Person * one, const Person * two) {
+        return one->getLastName() < two->getLastName();
+    });
 }
 
--- includes/DataBase.hpp
+++ includes/DataBase.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <algorithm>
 #include "Student.hpp"
 #include "Employee.hpp"
 
