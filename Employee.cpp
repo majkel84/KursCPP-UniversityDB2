61e152f7e81b47d0f577ab5a6927b507c9cf73d4
Jakub Zych <jakub.zych@tieto.com>
2019-01-24 14:44:00 +0100

    Employee: Add Employee class body and header

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,26 @@
 #include "Employee.hpp"
+
+using namespace std;
+
+Employee::Employee(const string name, 
+                   const string lastName, 
+                   const int64_t pesel, 
+                   const bool gender,     // true - male, false - female
+                   const string address,
+                   const int income) 
+    : Person(name, lastName, pesel, gender, address)
+    , income_(income) {}
+    
+int Employee::getIncome() const {
+    return income_;
+}
+
+void Employee::setIncome(const int newIncome) {
+    income_ = newIncome;
+}
+
+void Employee::show() {
+    cout << name_ << " " << lastName_ << " " << pesel_ << " " 
+         << (gender_ ? "male " : "female ") << address_ << " "
+         << income_ << endl;
+}
---
+++ includes/Employee.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include "Person.hpp"
+
+using namespace std;
+
+class Employee : public Person {
+    public:
+        Employee(const string name, 
+                 const string lastName, 
+                 const int64_t pesel, 
+                 const bool gender,     // true - male, false - female
+                 const string address,
+                 const int income); 
+               
+        virtual void show();
+        int getIncome() const;
+        void setIncome(const int newIncome);
+        
+    private:
+        int income_;
+};
