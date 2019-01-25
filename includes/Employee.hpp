095f8243ba0299f6758c1bf8ea48beb80a5381cf
Jakub Zych <jakub.zych@tieto.com>
2019-01-25 14:16:12 +0100

    Person: Add gender as enum

--- includes/Employee.hpp
+++ includes/Employee.hpp
@@ -6,17 +6,17 @@ using namespace std;
 
 class Employee : public Person {
     public:
-        Employee(const string name, 
-                 const string lastName, 
-                 const int64_t pesel, 
-                 const bool gender,     // true - male, false - female
+        Employee(const string name,
+                 const string lastName,
+                 const int64_t pesel,
+                 const Gender gender,
                  const string address,
-                 const int income); 
-               
+                 const int income);
+
         virtual void show();
         int getIncome() const;
         void setIncome(const int newIncome);
-        
+
     private:
         int income_;
 };
