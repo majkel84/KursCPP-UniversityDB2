cc531275b5542fb2cb7ad4b5742d4123ce215347
tomasz0 <46450682+tomasz0@users.noreply.github.com>
2019-01-26 01:43:49 +0100

    Update DataBase.cpp

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,4 +1,23 @@
 #include "DataBase.hpp"
+#include <iostream>
+#include <string>
+#include <iomanip>
+#inlcude <fstream>
 
-ostream& operator << (ostream& stream_out, Person p)
+using namespace std;
+
+//overloading of << operator 
+ostream& operator << (ostream& stream_out, Person p) 
+{  
+  stream_out << p.name << " " << p.lastName << " " << p.pesel << " " << p.gender << " " << p.adress << " " << p.income;
+  return stream_out; 
+}
   
+//overloading of >> operator 
+istream& operator >> (istream& stream_in, Person &p) 
+{  
+  stream_in >> p.name >> " " >> p.lastName >> " " >> p.pesel >> " " >> p.gender >> " " >> p.adress >> " " >> p.income;
+  return stream_in; 
+}
+  
+
