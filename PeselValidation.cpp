33506046b7eec7f28270998ce4dd026e2905d907
Jakub Zych <jzych89@gmail.com>
2019-01-26 12:09:15 +0100

    PeselValidation: Add isCheckSumValid function

--- PeselValidation.cpp
+++ PeselValidation.cpp
@@ -10,4 +10,20 @@ int32_t getSerialNo(int64_t pesel);
 
 Gender getGender(int32_t serial_no);
 
-PeselError isCheckSumValid(int64_t pesel);
+PeselError isCheckSumValid(int64_t pesel) {
+    int a = pesel / 10000000000 % 10;
+    int b = pesel / 1000000000 % 10;
+    int c = pesel / 100000000 % 10;
+    int d = pesel / 10000000 % 10;
+    int e = pesel / 1000000 % 10;
+    int f = pesel / 100000 % 10;
+    int g = pesel / 10000 % 10;
+    int h = pesel / 1000 % 10;
+    int i = pesel / 100 % 10;
+    int j = pesel / 10 % 10;
+    int k = pesel / 1 % 10;
+
+    int checksum = (9*a + 7*b + 3*c + d + 9*e + 7*f + 3*g + h + 9*i + 7*j) % 10;
+
+    return (checksum == k) ? PeselError::ok_success : PeselError::invalid_checksum;
+}
