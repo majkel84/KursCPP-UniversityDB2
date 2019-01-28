f0497a4d81c194d5f028153ff180a2db24d3b84d
Jakub Zych <jzych89@gmail.com>
2019-01-28 18:44:08 +0100

    Database: Fix pesel type in .hpp

--- includes/DataBase.hpp
+++ includes/DataBase.hpp
@@ -10,20 +10,19 @@ using Persons = vector<Person*>;
 class Database {
     public:
         void addItem(Person * person);
-        void removeByPESEL(const uint32_t pesel);
-        void modifyIncome(const uint newIncome, const uint32_t pesel);
-        void modifyAddress(const string newAddress, const uint32_t pesel);
+        void removeByPESEL(const int64_t pesel);
+        void modifyIncome(const uint newIncome, const int64_t pesel);
+        void modifyAddress(const string newAddress, const int64_t pesel);
         void sortByLastname();
         void sortByPESEL();
         void sortByIncome();
         void searchByLastname(const string lastname, Person* item);
-        void searchByPESEL(const uint32_t pesel, Person* item);
+        void searchByPESEL(const int64_t pesel);
         void showAll();
-        bool validatePESEL(uint32_t pesel);
+        bool validatePESEL(int64_t pesel);
         bool loadFromFile(const string filename);
         bool saveToFile(const string filename);
-        
+
     private:
         Persons db_;
 };
-        
