254d36729bfa3c92a20467daad6ad6c573375535
Jakub Zych <jakub.zych@tieto.com>
2019-01-28 13:40:34 +0100

    Main: Add assert section with Pesel validation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include "DataBase.hpp"
 
 using namespace std;
@@ -36,7 +37,16 @@ int main() {
     // Clear terminal
     cout << "\033[2J\033[1;1H";
 
-    do {   
+    // Assert section
+    assert(isPeselValid(std::stoll("12345678903"), Gender::Female) == PeselError::invalid_month);
+    assert(isPeselValid(std::stoll("12325678901"), Gender::Female) == PeselError::invalid_day);
+    assert(isPeselValid(std::stoll("03222978900"), Gender::Female) == PeselError::invalid_day);
+    assert(isPeselValid(std::stoll("04222978914"), Gender::Female) == PeselError::invalid_gender);
+    assert(isPeselValid(std::stoll("04222978907"), Gender::Male) == PeselError::invalid_gender);
+    assert(isPeselValid(std::stoll("04222978901"), Gender::Female) == PeselError::invalid_checksum);
+    assert(isPeselValid(std::stoll("04222978907"), Gender::Female) == PeselError::valid_ok);
+
+    do {
         menuShow();
         cin >> option;
 
@@ -44,43 +54,43 @@ int main() {
             case MENU_SHOW :
                 cout << "Show students list" << endl;
                 break;
-            case MENU_NEW_STUDENT : 
+            case MENU_NEW_STUDENT :
                 cout << "Add new student" << endl;
                 break;
-            case MENU_NEW_EMPLOYEE : 
+            case MENU_NEW_EMPLOYEE :
                 cout << "Add new employee" << endl;
                 break;
-            case MENU_SEARCH_NAME : 
+            case MENU_SEARCH_NAME :
                 cout << "Search by last name" << endl;
                 break;
-            case MENU_SEARCH_PESEL : 
+            case MENU_SEARCH_PESEL :
                 cout << "Search by PESEL" << endl;
                 break;
-            case MENU_REMOVE_PESEL : 
+            case MENU_REMOVE_PESEL :
                 cout << "Remove by PESEL" << endl ;
                 break;
-            case MENU_MODIFY_PESEL : 
+            case MENU_MODIFY_PESEL :
                 cout << "Modify emplyee by PESEL" << endl;
                 break;
-            case MENU_SORT_SALARY : 
+            case MENU_SORT_SALARY :
                 cout << "Sort by salary" << endl;
                 break;
-            case MENU_SORT_PESEL : 
+            case MENU_SORT_PESEL :
                 cout << "Sort by PESEL" << endl;
                 break;
-            case MENU_SORT_NAME : 
+            case MENU_SORT_NAME :
                 cout << "Sort by last name" << endl;
                 break;
-            case MENU_EXIT : 
+            case MENU_EXIT :
                 cout << "Exit (save to file)" << endl;
                 break;
-            default : 
+            default :
                 cout << "Unknown option " << option << "!" << endl;
                 break;
         }
-        
+
         cout << endl;
     } while (option != MENU_EXIT);
-    
+
     return 0;
 }
